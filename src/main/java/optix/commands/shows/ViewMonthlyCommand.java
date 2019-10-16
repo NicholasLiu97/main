ecc100ea8dbfcf9add323ef425a0aea2b054b021
Nicholas Liu <nicholasliu097@gmail.com>
2019-10-16 21:36:04 +0800

    added ViewMonthlyCommand.java Format: view-monthly may 2020

--- src/main/java/optix/commands/shows/ViewMonthlyCommand.java
+++ src/main/java/optix/commands/shows/ViewMonthlyCommand.java
@@ -11,24 +11,28 @@ import optix.exceptions.OptixInvalidDateException;
 import optix.ui.Ui;
 
 import java.time.LocalDate;
-import java.util.Iterator;
 import java.util.Map;
 
 public class ViewMonthlyCommand extends Command {
 
     private String month;
     private String year;
-    private String[] monthList = {"january", "february", "march", "april", "may", "june", "july", "august", "september",
-            "october", "november", "december"};
+    private static String[] monthList = {"january", "february", "march", "april", "may", "june", "july", "august",
+        "september", "october", "november", "december"};
 
-    private static final String MESSAGE_NO_SHOW_FOUND = "☹ OOPS!!! There are no shows in %1$s %2$s\n";
+    private static final String MESSAGE_NO_SHOW_FOUND = "☹ OOPS!!! There are no shows in %1$s %2$s.\n";
 
-    private static final String MESSAGE_SUCCESSFUL = "The amount earned in %1$s %2$s is %3$s\n";
+    private static final String MESSAGE_NOT_YET_CALCULATED = "☹ OOPS!!! The earnings for %1$s %2$s has not been"
+            + " calculated. Try other months.\n";
+
+    private static final String MESSAGE_SUCCESSFUL_ARCHIVE = "The amount earned in %1$s %2$s is %3$s.\n";
+
+    private static final String MESSAGE_SUCCESSFUL_LIST = "The projected earnings for %1$s %2$s is %3$s.\n";
 
     /**
      * Views the profit for a certain month.
-     * @param month month to query for.
-     * @param year  year to query for.
+     * @param month Month to query for.
+     * @param year  Year to query for.
      */
     public ViewMonthlyCommand(String month, String year) {
         this.month = month.trim().toLowerCase();
@@ -37,6 +41,11 @@ public class ViewMonthlyCommand extends Command {
         System.out.println("the year is " + year);
     }
 
+    /**
+     * Converts month into numerical number.
+     * @param month the month.
+     * @return an integer corresponding to the month.
+     */
     private int parseInt(String month) {
         int mth = 1;
 
@@ -51,9 +60,14 @@ public class ViewMonthlyCommand extends Command {
         return mth;
     }
 
-    private boolean isValidMonth(String month) {
+    /**
+     * Checks if the String entered for month is valid.
+     * @param description The String to be checked.
+     * @return True if the description matches any of the months.
+     */
+    private boolean isValidMonth(String description) {
         for (int i = 0; i < monthList.length; i++) {
-            if (month.equals(monthList[i])) {
+            if (description.equals(monthList[i])) {
                 return true;
             }
         }
@@ -61,6 +75,12 @@ public class ViewMonthlyCommand extends Command {
         return false;
     }
 
+    /**
+     * Checks if the String entered for year is valid.
+     * @param year The String to be checked.
+     * @return True if the String can be parsed into an integer.
+     * @throws NumberFormatException if the String cannot be parsed into an integer.
+     */
     private boolean isValidYear(String year) {
         try {
             Integer.parseInt(year);
@@ -71,60 +91,100 @@ public class ViewMonthlyCommand extends Command {
         return true;
     }
 
+    /**
+     * Calculates the earnings for a certain month from the archive file.
+     * @param model The model containing the shows stored in the archive file.
+     * @param mth The month in numerical form.
+     * @param yr The year.
+     * @return A message String that contains the profit to show to the user.
+     */
+    private String findFromArchive(Model model, int mth, int yr) {
+        String message = "";
+        double profit = 0.0;
+
+        ShowHistoryMap shows = model.getShowsHistory();
+        ShowHistoryMap showsWanted = new ShowHistoryMap();
+        LocalDate key;
+        Show value;
+        for (Map.Entry<LocalDate, Show> entry : shows.entrySet()) {
+            key = entry.getKey();
+            value = entry.getValue();
+            showsWanted.put(key, value);
+        }
+
+        showsWanted.entrySet().removeIf(entry -> entry.getKey().getMonthValue() != mth
+                || entry.getKey().getYear() != yr);
+
+        if (showsWanted.isEmpty()) {
+            message = String.format(MESSAGE_NO_SHOW_FOUND, month, year);
+        } else {
+            for (Map.Entry<LocalDate, Show> entry : showsWanted.entrySet()) {
+                profit += entry.getValue().getProfit();
+            }
+            message = String.format(MESSAGE_SUCCESSFUL_ARCHIVE, month, year, profit);
+        }
+
+        return message;
+    }
+
+    /**
+     * Calculates the earnings for a certain month from the Optix file.
+     * @param model The model containing the shows stored in the Optix file.
+     * @param mth The month in numerical form.
+     * @param yr The year.
+     * @return A message String that contains the profit to show to the user.
+     */
+    private String findFromList(Model model, int mth, int yr) {
+        String message = "";
+        double profit = 0.0;
+
+        ShowMap shows = model.getShows();
+        ShowMap showsWanted = new ShowMap();
+        LocalDate key;
+        Theatre value;
+        for (Map.Entry<LocalDate, Theatre> entry : shows.entrySet()) {
+            key = entry.getKey();
+            value = entry.getValue();
+            showsWanted.put(key, value);
+        }
+
+        showsWanted.entrySet().removeIf(entry -> entry.getKey().getMonthValue() != mth
+                || entry.getKey().getYear() != yr);
+
+        if (showsWanted.isEmpty()) {
+            message = String.format(MESSAGE_NO_SHOW_FOUND, month, year);
+        } else {
+            for (Map.Entry<LocalDate, Theatre> entry : showsWanted.entrySet()) {
+                profit += entry.getValue().getProfit();
+            }
+            message = String.format(MESSAGE_SUCCESSFUL_LIST, month, year, profit);
+        }
+
+        return message;
+    }
+
     @Override
     public void execute(Model model, Ui ui, Storage storage) {
         String message = "";
+        int mth = parseInt(month);
+        int yr = Integer.parseInt(year);
 
         try {
             if (!isValidMonth(month) || !isValidYear(year)) {
                 throw new OptixInvalidDateException();
             }
 
-            int mth = parseInt(month);
-            int yr = Integer.parseInt(year);
-
-            System.out.println("the mth is " + mth);
-            System.out.println("the yr is " + yr);
-            double profit = 0.0;
-
-            ShowHistoryMap shows = model.getShowsHistory();
-            ShowHistoryMap showsWanted = new ShowHistoryMap();
-
-            LocalDate key;
-            Show value;
-
-            for (Map.Entry<LocalDate, Show> entry : shows.entrySet()) {
-                key = entry.getKey();
-                value = entry.getValue();
-                showsWanted.put(key, value);
-            }
-
-            System.out.println(showsWanted.size());
-
-            /*
-            Iterator<Map.Entry<LocalDate, Theatre>> iter = shows.entrySet().iterator();
-            while (iter.hasNext()) {
-                //Map.Entry<LocalDate, Theatre> entry = iter.next();
-                //remove show from the show list if year and month does not match
-                shows.entrySet().removeIf(entry -> entry.getKey().getMonthValue() != mth
-                        || entry.getKey().getYear() != yr);
-                System.out.println(shows.size());
-            }
-             */
-
-            showsWanted.entrySet().removeIf(entry -> entry.getKey().getMonthValue() != mth
-                    || entry.getKey().getYear() != yr);
-
-            System.out.println("test line");
-            if (showsWanted.isEmpty()) {
-                message = String.format(MESSAGE_NO_SHOW_FOUND, month, year);
-            } else {
-                for (Map.Entry<LocalDate, Show> entry : showsWanted.entrySet()) {
-                    profit += entry.getValue().getProfit();
+            if (yr <= storage.getToday().getYear()) {
+                if (mth < storage.getToday().getMonthValue()) {
+                    message = findFromArchive(model, mth, yr);
+                } else if (mth == storage.getToday().getMonthValue()) {
+                    message = String.format(MESSAGE_NOT_YET_CALCULATED, month, year);
+                } else {
+                    message = findFromList(model, mth, yr);
                 }
-                message = String.format(MESSAGE_SUCCESSFUL, month, year, profit);
+            } else {
+                message = findFromList(model, mth, yr);
             }
-
         } catch (OptixInvalidDateException e) {
             message = e.getMessage();
         } finally {
